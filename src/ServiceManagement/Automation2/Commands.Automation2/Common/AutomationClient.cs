2d84a49c850b888b4a8b226561f70217fc6e18b5
elvg <elvg@microsoft.com>
2014-12-09 14:43:58 -0800

    get-azureautomationschedule implementation using new Automation SDK

---
+++ src/ServiceManagement/Automation2/Commands.Automation2/Common/AutomationClient.cs
@@ -0,0 +1,128 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Microsoft.Azure.Commands.Automation.Model;
+using Microsoft.Azure.Commands.Automation.Properties;
+using Microsoft.Azure.Management.Automation;
+using Microsoft.WindowsAzure.Commands.Common;
+using Microsoft.WindowsAzure.Commands.Common.Models;
+using Newtonsoft.Json;
+
+namespace Microsoft.Azure.Commands.Automation.Common
+{
+    using AutomationManagement = Management.Automation;
+
+    public class AutomationClient : IAutomationClient
+    {
+        private readonly AutomationManagement.IAutomationManagementClient automationManagementClient;
+
+        // Injection point for unit tests
+        public AutomationClient()
+        {
+        }
+
+        public AutomationClient(AzureSubscription subscription)
+            : this(subscription,
+            AzureSession.ClientFactory.CreateClient<AutomationManagement.AutomationManagementClient>(subscription, AzureEnvironment.Endpoint.ServiceManagement))
+        {
+        }
+
+        public AutomationClient(
+            AzureSubscription subscription,
+            AutomationManagement.IAutomationManagementClient automationManagementClient)
+        {
+            Requires.Argument("automationManagementClient", automationManagementClient).NotNull();
+
+            this.Subscription = subscription;
+            this.automationManagementClient = automationManagementClient;
+        }
+
+        public AzureSubscription Subscription { get; private set; }
+
+        #region Schedule Operations
+
+        public Schedule GetSchedule(string automationAccountName, string scheduleName)
+        {
+            AutomationManagement.Models.Schedule scheduleModel = this.GetScheduleModel(automationAccountName, scheduleName);
+            return this.CreateScheduleFromScheduleModel(scheduleModel);
+        }
+
+        public IEnumerable<Schedule> ListSchedules(string automationAccountName)
+        {
+            IList<AutomationManagement.Models.Schedule> scheduleModels = AutomationManagementClient.ContinuationTokenHandler(
+                skipToken =>
+                {
+                    var response = this.automationManagementClient.Schedules.List(
+                        automationAccountName);
+                    return new ResponseWithSkipToken<AutomationManagement.Models.Schedule>(
+                        response, response.Schedules);
+                });
+
+            return scheduleModels.Select(this.CreateScheduleFromScheduleModel);
+        }
+
+        #endregion
+
+        #region Private Methods
+
+        private Schedule CreateScheduleFromScheduleModel(AutomationManagement.Models.Schedule schedule)
+        {
+            Requires.Argument("schedule", schedule).NotNull();
+
+            return new Schedule(schedule);
+
+            /*if (schedule.ScheduleType == AutomationManagement.Models.ScheduleType.DailySchedule)
+            {
+                return new DailySchedule(schedule);
+            }
+            else if (schedule.ScheduleType == AutomationManagement.Models.ScheduleType.HourlySchedule)
+            {
+                return new HourlySchedule(schedule);
+            }
+            else
+            {
+                return new OneTimeSchedule(schedule);
+            }*/
+        }
+
+        private AutomationManagement.Models.Schedule GetScheduleModel(string automationAccountName, string scheduleName)
+        {
+            AutomationManagement.Models.Schedule scheduleModel = this.automationManagementClient.Schedules.Get(
+                automationAccountName,
+                scheduleName)
+                .Schedule;
+
+            if (scheduleModel == null)
+            {
+                throw new ResourceNotFoundException(typeof(Schedule), string.Format(CultureInfo.CurrentCulture, Resources.ScheduleNotFound, scheduleName));
+            }
+
+            return scheduleModel;
+        }
+
+        private string FormatDateTime(DateTime dateTime)
+        {
+            return string.Format(CultureInfo.InvariantCulture, "{0:O}", dateTime.ToUniversalTime());
+        }
+
+        #endregion
+    }
+}
---
+++ src/ServiceManagement/Automation2/Commands.Automation2/Model/Schedule.cs
@@ -0,0 +1,112 @@
+﻿// ----------------------------------------------------------------------------------
+//
+// Copyright Microsoft Corporation
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// http://www.apache.org/licenses/LICENSE-2.0
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// ----------------------------------------------------------------------------------
+
+using Microsoft.Azure.Commands.Automation.Common;
+using Microsoft.Azure.Commands.Automation.Properties;
+using System;
+
+namespace Microsoft.Azure.Commands.Automation.Model
+{
+    /// <summary>
+    /// The Schedule.
+    /// </summary>
+    public class Schedule
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Schedule"/> class.
+        /// </summary>
+        /// <param name="schedule">
+        /// The schedule.
+        /// </param>
+        public Schedule(Azure.Management.Automation.Models.Schedule schedule)
+        {
+            Requires.Argument("schedule", schedule).NotNull();
+            //this.AccountId = new Guid(schedule.AccountId);
+            this.Name = schedule.Name;
+            this.Description = schedule.Description;
+            this.StartTime = schedule.StartTime.ToLocalTime();
+            this.ExpiryTime = schedule.ExpiryTime.ToLocalTime();
+            this.CreationTime = schedule.CreationTime.ToLocalTime();
+            this.LastModifiedTime = schedule.LastModifiedTime.ToLocalTime();
+            this.IsEnabled = schedule.IsEnabled;
+            this.NextRun = schedule.NextRun.HasValue
+                               ? schedule.NextRun.Value.ToLocalTime()
+                               : this.NextRun;
+            this.Interval = schedule.Interval.Value;
+            this.Frequency = (ScheduleFrequency) Enum.Parse(typeof(ScheduleFrequency), schedule.Frequency);
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="HourlySchedule"/> class.
+        /// </summary>
+        public Schedule()
+        {
+        }
+
+        /// <summary>
+        /// Gets or sets the account id.
+        /// </summary>
+        public Guid AccountId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the description.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Gets or sets the start time.
+        /// </summary>
+        public DateTimeOffset StartTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the expiry time.
+        /// </summary>
+        public DateTimeOffset? ExpiryTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the creation time.
+        /// </summary>
+        public DateTimeOffset CreationTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the last modified time.
+        /// </summary>
+        public DateTimeOffset LastModifiedTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether is enabled.
+        /// </summary>
+        public bool IsEnabled { get; set; }
+
+        /// <summary>
+        /// Gets or sets the next run.
+        /// </summary>
+        public DateTimeOffset? NextRun { get; set; }
+
+        /// <summary>
+        /// Gets or sets the schedule interval.
+        /// </summary>
+        public byte? Interval { get; set; }
+
+        /// <summary>
+        /// Gets or sets the schedule frequency.
+        /// </summary>
+        public ScheduleFrequency Frequency { get; set; }
+    }
+}
